c9d3f60564fed61dcc68c5005f73941f19c512fa
vkc2012 <38739048+vkc2012@users.noreply.github.com>
2018-06-16 12:35:02 -0600

    push it yet again

--- cit230/assignments/lesson-08/scripts/currentdate.js
+++ cit230/assignments/lesson-08/scripts/currentdate.js
@@ -11,7 +11,25 @@ function currentDay() {
     
     var n = weekday[date.getDay()];
     
-    document.getElementById("day").innerHTML = n;
-
-    document.getElementById("currentDate").innerHTML = date.toDateString();
+    var d = date.getDate();
+    
+    var month = [];
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+    
+    var m = month[date.getMonth()];
+    
+    var y = date.getFullYear();
+    
+    document.getElementById("day").innerHTML = n + " " + d + " " + m + " " + y;
 }
