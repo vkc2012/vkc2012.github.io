8b4e6ce7a2557ee309402dd393d8706db530c48a
vkc2012 <38739048+vkc2012@users.noreply.github.com>
2018-06-20 14:24:14 -0600

    Lesson 09 First Push

---
+++ cit230/assignments/Lesson-09/scripts/currentdate.js
@@ -0,0 +1,35 @@
+function currentDay() {
+    var date = new Date();
+    var weekday = [];
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    
+    var n = weekday[date.getDay()];
+    
+    var d = date.getDate();
+    
+    var month = [];
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+    
+    var m = month[date.getMonth()];
+    
+    var y = date.getFullYear();
+    
+    document.getElementById("day").innerHTML = n + " " + d + " " + m + " " + y;
+}
