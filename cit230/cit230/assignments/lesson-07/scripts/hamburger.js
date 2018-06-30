8d9e337ae9258dffed5cde85b8faa7eb3f0461d1
vkc2012 <38739048+vkc2012@users.noreply.github.com>
2018-06-29 23:42:58 -0600

    L10 Push again

--- cit230/cit230/assignments/lesson-07/scripts/hamburger.js
+++
@@ -1,3 +0,0 @@
-function toggleHam() {
-	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-}
--- cit230/cit230/assignments/lesson_06/scripts/hamburger.js
+++
@@ -1,9 +0,0 @@
-
-<div class = "topnav" id="myTopnav">
-    <a href="#" class="active">Home</a>
-    <a href="#" class="active">Franklin</a>
-    <a href="#"class="active">Greenville</a>
-    <a href="#"class="active">Springfield</a>
-    <a href="#"class="active">Storm Center</a>
-    <a href="#"class="active">Gallery</a>
-</div>
