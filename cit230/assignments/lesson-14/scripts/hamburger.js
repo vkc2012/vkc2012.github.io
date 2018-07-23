7acae2fa9971248a96cef6d87e0c0496b7684bc8
vkc2012 <38739048+vkc2012@users.noreply.github.com>
2018-07-23 07:12:05 -0600

    Final Project Push it

---
+++ cit230/assignments/lesson-14/scripts/hamburger.js
@@ -0,0 +1,8 @@
+function toggleHam() {
+    var x = document.getElementById("hamburger");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
